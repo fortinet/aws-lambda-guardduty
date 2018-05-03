d01ff90425acd40dfd5eb40a37a815a4f06b91fc
Jamie Pate <jpate@fortinet.com>
2018-05-03 12:11:29 -0700

    Remove node modules and add packaging script Add travis.yml
    Change-Id: I6fa75181792d7aa32541738078bbf75436288efe

---
+++ scripts/install_notes.js
@@ -0,0 +1,6 @@
+require('dpl');
+var notes = [
+    'run "npm run build" to create the distribution zip file',
+    'run "npm run deploy" to deploy to AWS directly'
+];
+console.log(notes.join('\n') + '\n');
---
+++ scripts/make_dist.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+console.log('Making distribution zip package');
+var pkg = require('../package.json'),
+    os = require('os'),
+    fs = require('fs'),
+    path = require('path'),
+    rimraf = require('rimraf');
+
+process.env.TMPDIR = fs
+    .mkdtempSync(path.join(process.env.TMPDIR || os.tmpdir(), pkg.name + '-')) + path.sep;
+
+// Shorter version of node_modules/dpl/dpl.js which avoids the 'upload' phase
+
+var dpl = require('dpl/lib/index.js');
+// 'upload' into the ./dist folder instead.
+dpl.upload = function() {
+    var fileName = pkg.name + '.zip';
+    var zipFile = path.normalize(process.env.TMPDIR + fileName);
+    var distDir = path.normalize(path.join(__dirname, '..', 'dist'));
+    try { fs.mkdirSync(distDir); } catch (ex) {}
+    copyFile(zipFile, path.join(distDir, fileName), function() {
+        rimraf.sync(path.dirname(zipFile));
+        console.log('zipped to ' + path.relative(process.cwd(), path.join(distDir, fileName)));
+    });
+};
+require('dpl/dpl.js');
+
+function copyFile(src, dest, cb) {
+    fs.createReadStream(src).pipe(fs.createWriteStream(dest))
+        .on('error', console.error)
+        .on('close', cb);
+}
